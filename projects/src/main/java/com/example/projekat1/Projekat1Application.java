a8eb8b068e6b5025786baf451e0bda988debb143
tk0p3 <petkovic96@gmail.com>
2023-01-16 15:20:54 +0100

    gg

---
+++ projects/src/main/java/com/example/projekat1/Projekat1Application.java
@@ -0,0 +1,13 @@
+package com.example.projekat1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekat1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekat1Application.class, args);
+    }
+
+}
